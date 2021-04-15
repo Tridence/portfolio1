d09e3cff245a75769d284dd03896d264cd8431f4
Wanjala <48374660+Tridence@users.noreply.github.com>
2021-04-15 13:51:53 +0300

    Create email.php

---
+++ email.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("brianwanjalabarasa@gmail.com", "Message from Portfolio",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
